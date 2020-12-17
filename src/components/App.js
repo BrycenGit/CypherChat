91ac6a36b8e64b5d66ace8dc382f3dcd4c689643
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-16 19:36:45 -0800

    Refactor firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,15 @@
 import '../App.css';
 import React from 'react';
-import 'firebase/auth'
-import 'firebase/firestore'
-import { Provider } from 'react-redux';
-import rootReducer from '../reducers/index';
-import firebase from '../firebase';
-import { createStore } from 'redux';
-import { createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
-import TodosControl from './TodosControl.js'
+import MessagesControl from './MessagesControl.js'
 
-const rrfConfig = {
-  userProfile: 'users',
-  useFireStoreForProfile: true,
-}
 
-// const initialState = {}
-
-const store = createStore(rootReducer)
-
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance
-}
 
 function App() {
   
   return (
-    <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <TodosControl />
-      </ReactReduxFirebaseProvider>
-    </Provider>
+    <>
+      <MessagesControl />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,36 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'firebase/database';
+import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { Provider } from 'react-redux';
+import firebase from './firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import { createStore } from 'redux';
+
+const rrfConfig = {
+  userProfile: 'users',
+  useFireStoreForProfile: true,
+}
+
+// const initialState = {}
+
+const store = createStore(rootReducer)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>,
   document.getElementById('root')
 );
 
