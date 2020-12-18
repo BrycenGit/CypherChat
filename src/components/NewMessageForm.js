f7ab7bcbbb04aac766f91edd5f139776df409d4a
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-18 14:34:18 -0800

    working on dropdown for selected users

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -11,12 +11,6 @@ function NewMessageForm(props) {
 
   const { currentUser, recipientEmail } = props;
 
-  useFirestoreConnect([
-    {
-      collection: "users",
-    },
-  ]);
-
   const emailArray = [currentUser.email, recipientEmail];
   const sortedEmails = emailArray.sort((a, b) => a.localeCompare(b));
   const collectionName = sortedEmails.join("-");
@@ -29,9 +23,7 @@ function NewMessageForm(props) {
     e.preventDefault();
 
     await chatRef.add({
-      // title: e.target.title.value,
       body: e.target.body.value,
-      // chat: [sortedEmails.join('-')],
       sender: currentUser.email,
       recipient: recipientEmail,
       timeOpen: firestore.FieldValue.serverTimestamp(),
@@ -43,10 +35,6 @@ function NewMessageForm(props) {
     <>
       <h1>New Message Form</h1>
       <form onSubmit={addMessageToFirestore}>
-        {/* <label htmlFor="title">Title</label>
-        <input name="title" type="text" />
-        <br /> */}
-        {/* <label htmlFor="body">Body</label> */}
         <div class="flex-form">
           <input
             name="body"
@@ -55,7 +43,6 @@ function NewMessageForm(props) {
               setFormValue(e1.target.value);
             }}
           />
-
           <button type="submit">Submit</button>
         </div>
       </form>
