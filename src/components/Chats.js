b3525373a445c21178d651d609860623ddaf95d3
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-17 16:50:35 -0800

    i believe i got the selct chat hooks to work

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,6 +2,7 @@ import { useFirestore, useFirestoreConnect, isLoaded } from 'react-redux-firebas
 import { useSelector } from 'react-redux'
 import React, { useState } from 'react';
 import Message from './Message'
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function Chats(props) {
   
@@ -9,36 +10,47 @@ function Chats(props) {
   const { user } = props;
   const [chat, setChat] = useState([])
   
+  const usersRef = firestore.collection('users')
+  const [usersList] = useCollectionData(usersRef, {idField: 'id'});
+  // useFirestoreConnect([{ 
+  //   collection: 'users'
+  // }])
+  // const usersList = useSelector(state => state.firestore.ordered.users);
+  const [recip, setRecip] = useState(null)
+
+  const messagesRef = firestore.collection('messages').where("chat", "in",  [[user.email, recip], [ recip, user.email]] )
+  const [messages] = useCollectionData(messagesRef, {idField: 'id'});
   
-  useFirestoreConnect([{ 
-    collection: 'users'
-  }])
-  const usersList = useSelector(state => state.firestore.ordered.users);
+  // const getMessages = (recip) => {
+  //   var messages = [];
+  //   const messagesRef = firestore.collection('messages').where("chat", "in",  [[user.email, recip], [ recip, user.email]] )
+  //   messagesRef.onSnapshot((q) => {
+  //       q.forEach(function(doc) {
+  //           messages.push(doc.data());
+  //       });
+  //   });
+  //   return messages;
+  // }
   
-  const messagesRef = firestore.collection('messages')
-  const getMessages = (recip) => {
-    var messages = [];
-    messagesRef.where("chat", "in",  [[user.email, recip], [ recip, user.email]] )
-    .onSnapshot((q) => {
-        q.forEach(function(doc) {
-            messages.push(doc.data());
-        });
-    });
-    return messages;
-  }
-  
-  function selectChat(e) {
-    e.preventDefault();
-    const recipient = e.target.recipient.value
-    return getMessages(recipient);
+  // function selectChat(e) {
+  //   e.preventDefault();
+  //   const recipient = e.target.recipient.value
+  //   return getMessages(recipient);
+  // }
+  const doSomething = (e) => {
+    e.preventDefault()
+    alert('clicked me')
+    
+    setRecip(e.target.recipient.value)
+    
   }
 
-  if (isLoaded(usersList)) {
+  if ((isLoaded(usersList)) && (isLoaded(messages))) {
     console.log(chat)
     return (
       <>
       <h1>Chats</h1>
-      <form onSubmit={(e)=> setChat(selectChat(e))}>
+      <form onSubmit={doSomething}>
         
         {usersList.map((user)=>{
           return (<div key={user.id}>
@@ -52,7 +64,7 @@ function Chats(props) {
         <button type="submit">Submit</button>
       </form>
       <h1>message List</h1>
-        {chat.map((msg) => {
+        {messages && messages.map((msg) => {
           console.log('hello')
           return <Message user={user} sender={msg.sender} recipient={msg.recipient} title={msg.title} body={msg.body} id={msg.id} key={msg.id} />
         })}
