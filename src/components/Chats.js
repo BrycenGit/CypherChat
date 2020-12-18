9d2be85d981f405fa5dccdb7e045bb838abd51e7
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-18 15:09:14 -0800

    implement dropdown with users to select user you are chatting

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -12,15 +12,7 @@ function Chats(props) {
     .where("email", "!=", currentUser.email);
   const [usersList] = useCollectionData(usersRef, { idField: "id" });
 
-  const doSomething = (e) => {
-    e.preventDefault();
-    handleSelectChat(e.target.recipient.value);
-  };
-
   if (isLoaded(usersList)) {
-    // const filteredUsers = usersList.filter(
-    //   (user) => user.email !== currentUser.email
-    // );
     return (
       <>
         <h1>Chats</h1>
@@ -28,22 +20,14 @@ function Chats(props) {
           <button class="dropbtn">Dropdown</button>
           <div class="dropdown-content">
             {usersList.map((user) => {
-              return <a href="#">{user.username}</a>;
+              return (
+                <a onClick={() => handleSelectChat(user.email)}>
+                  {user.username}
+                </a>
+              );
             })}
           </div>
         </div>
-        <form onSubmit={(e) => doSomething(e)}>
-          {usersList.map((user) => {
-            return (
-              <div key={user.id}>
-                <label htmlFor={user.email}>{user.email}</label>
-                <input name="recipient" type="radio" value={user.email} />
-                <br />
-              </div>
-            );
-          })}
-          <button type="submit">Submit</button>
-        </form>
       </>
     );
   } else {
