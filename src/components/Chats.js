9644913d7ee4d5316d6fa6f312a5df24cb05e031
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-18 14:08:19 -0800

    updated users qwquery for users list

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -5,51 +5,37 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 
 function Chats(props) {
   const firestore = useFirestore();
-  const { handleblankClick, handleSelectChat, currentUser } = props;
+  const { handleSelectChat, currentUser } = props;
 
-  const usersRef = firestore.collection("users");
+  const usersRef = firestore
+    .collection("users")
+    .where("email", "!=", currentUser.email);
   const [usersList] = useCollectionData(usersRef, { idField: "id" });
-
-  const [recip, setRecip] = useState(null);
-  const messagesRef = firestore.collection("messages").where("chat", "in", [
-    [currentUser.email, recip],
-    [recip, currentUser.email],
-  ]);
-  const [messages] = useCollectionData(messagesRef, { idField: "id" });
 
   const doSomething = (e) => {
     e.preventDefault();
-    setRecip(e.target.recipient.value);
     handleSelectChat(e.target.recipient.value);
   };
 
-  if (isLoaded(usersList) && isLoaded(messages)) {
-    const filteredUsers = usersList.filter(
-      (user) => user.email !== currentUser.email
-    );
+  if (isLoaded(usersList)) {
+    // const filteredUsers = usersList.filter(
+    //   (user) => user.email !== currentUser.email
+    // );
     return (
       <>
         <h1>Chats</h1>
         <form onSubmit={(e) => doSomething(e)}>
-          {filteredUsers.map((user) => {
+          {usersList.map((user) => {
             return (
               <div key={user.id}>
-                <input name="recipient" type="radio" value={user.email} />
                 <label htmlFor={user.email}>{user.email}</label>
+                <input name="recipient" type="radio" value={user.email} />
                 <br />
               </div>
             );
           })}
-
-          <br />
           <button type="submit">Submit</button>
         </form>
-
-        <button onClick={handleblankClick}>Blank</button>
-        {/* {messages && messages.map((msg) => {
-          console.log('hello')
-          return <Message user={currentUser} sender={msg.sender} recipient={msg.recipient} title={msg.title} body={msg.body} id={msg.id} key={msg.id} />
-        })} */}
       </>
     );
   } else {
