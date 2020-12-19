2b6ded6513a3aa9791cf0e5db9eb705a567492b8
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-18 16:32:10 -0800

    add scroll bar that scrolls to bottom when new message is added

--- src/components/MessagesList.js
+++ src/components/MessagesList.js
@@ -2,7 +2,7 @@ import { useFirestore, isLoaded } from "react-redux-firebase";
 import Message from "./Message";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import NewMessageForm from "./NewMessageForm";
-
+import { useRef } from "react";
 function MessagesList(props) {
   const firestore = useFirestore();
   const { user, recipientEmail } = props;
@@ -28,23 +28,25 @@ function MessagesList(props) {
           messageSent={messageSent}
         />
         <h1>message List</h1>
-        {messagesList &&
-          messagesList.map((msg) => {
-            return (
-              <div key={msg.id}>
-                <Message
-                  user={user}
-                  sender={msg.sender}
-                  recipient={msg.recipient}
-                  title={msg.title}
-                  body={msg.body}
-                  id={msg.id}
-                  key={msg.id}
-                />
-              </div>
-            );
-          })}
-        <div ref={dummy}></div>
+        <div className="message-box">
+          {messagesList &&
+            messagesList.map((msg) => {
+              return (
+                <div key={msg.id}>
+                  <Message
+                    user={user}
+                    sender={msg.sender}
+                    recipient={msg.recipient}
+                    title={msg.title}
+                    body={msg.body}
+                    id={msg.id}
+                    key={msg.id}
+                  />
+                </div>
+              );
+            })}
+          <div ref={dummy}></div>
+        </div>
       </>
     );
   } else {
