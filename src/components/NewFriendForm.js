6b142728870e99dc35da10ac724388874fbaa45b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-19 20:06:16 -0800

    reading documentation on passing objects through input fields

--- src/components/NewFriendForm.js
+++ src/components/NewFriendForm.js
@@ -1,7 +1,7 @@
 import { useFirestore } from "react-redux-firebase";
-
+import React from "react";
 const NewFriendForm = (props) => {
-  const { currentUser } = props;
+  const { currentUser, usersList } = props;
   const firestore = useFirestore();
   const currentUserRef = firestore
     .collection("users")
@@ -10,14 +10,24 @@ const NewFriendForm = (props) => {
 
   const addFriend = async (e) => {
     e.preventDefault();
-    await currentUserRef.doc(e.target.recipientEmail.value).set({
-      email: e.target.recipientEmail.value,
-    });
+    // await currentUserRef.doc(e.target.recipientEmail.value).set({
+    //   email: e.target.recipientEmail.value,
+    // });
+    await console.log(e.target.user.value);
   };
 
+  console.log(usersList);
   return (
     <>
       <form onSubmit={addFriend}>
+        {usersList.map((user) => {
+          return (
+            <div>
+              <input type="checkbox" value={user} name="user[]" />
+              <label htmlFor="user">{user.username}</label>
+            </div>
+          );
+        })}
         <input name="recipientEmail" type="text" placeholder="friends email" />
         <button type="Submit">Send Request</button>
       </form>
