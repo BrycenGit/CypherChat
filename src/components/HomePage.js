200bc386dea533b9789d719fb98b81a1a70f6e03
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-22 17:44:23 -0800

    fix whom

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,10 +5,9 @@ const HomePage = () => {
       <h1>Welcome To CypherChat!</h1>
       <p>
         At Cypher chat we take your privacy seriously. The private conversations
-        you have between yourself and whome ever you choose to converse with
-        will be encrypted and decypted clientside so that your private
-        conversations aren't floating freely in the cloud for wandering eyes to
-        see.
+        you have between yourself and whom ever you choose to converse with will
+        be encrypted and decypted clientside so that your private conversations
+        aren't floating freely in the cloud for wandering eyes to see.
       </p>
     </Home>
   );
